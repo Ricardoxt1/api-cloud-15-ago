dbed43a7d4f356f75f039ad172802485e4b02c41
Ricardoxt1 <ricardoalexandr_@hotmail.com>
2024-08-15 16:04:22 -0300

    fix, adicionando 2 novos end-point

---
+++ projetoaws (1)/projetoaws/src/test/java/com/ricardo/projetoaws/ProjetoawsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardo.projetoaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoawsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
